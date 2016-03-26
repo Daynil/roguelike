87d354052dc214dc9dd02e715d30c168f3e002be
Danny Libin <omegasol11@gmail.com>
2016-03-26 18:26:50 -0500

    changed to hosted spritesheet instead of webpacked

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 require('./styles');
-var sprites: any = document.createElement('sprites');
-sprites.src = require('./sprites.png');
+//var sprites: any = document.createElement('sprites');
+//sprites.src = require('./sprites.png');
 
 ReactDOM.render(<App />, document.getElementById('app'));
